241ca7663163d77b20e125d48fd172376a793151
jordanlkfung <jordanlkfung@gmail.com>
2025-05-28 20:10:31 -0700

    modified getLeaguesForUser to allow users to enter their username instead of userId

--- frontend/src/services/sleeper/leagues.ts
+++ frontend/src/services/sleeper/leagues.ts
@@ -1,8 +1,9 @@
 import { apiGet } from './apiClient';
-import type { League, Roster } from './types';
+import type { League, Roster, User } from './types';
 
-export const getLeaguesForUser = async (userId: string, season: string): Promise<League[]> => {
-    return apiGet<League[]>(`/user/${userId}/leagues/nba/${season}`);
+export const getLeaguesForUser = async (username: string, season: string): Promise<League[]> => {
+    const user: User = await apiGet(`/user/${username}`)
+    return apiGet<League[]>(`/user/${user.user_id}/leagues/nba/${season}`);
 }
 
 export const getRosters = async (leagueId: string): Promise<Roster[]> => {
