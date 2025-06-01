85591a77098f261ddeb36db715e25a861b6d1734
jordanlkfung <jordanlkfung@gmail.com>
2025-05-31 22:22:12 -0700

    added winston logger

--- backend/src/middleware/error_handler.ts
+++ backend/src/middleware/error_handler.ts
@@ -2,9 +2,10 @@ import { NextFunction, Request, Response } from "express";
 import { MongooseError } from "mongoose";
 import { HttpError } from "../constants/constants";
 import { AppError } from "../errors/app_error";
+import { logger } from "./logger";
 
 export function errorHandler(err: any, req: Request, res: Response, next: NextFunction) {
-
+    logger.error(err.message)
     if (err instanceof MongooseError) {
         res.status(HttpError.INTERNAL_SERVER_ERROR)
     }
--- backend/src/middleware/logger.ts
+++ backend/src/middleware/logger.ts
@@ -1,6 +1,23 @@
 import { Request, Response, NextFunction } from "express";
-
-export function logger(req: Request, res: Response, next: NextFunction) {
-    console.log(`${req.method} ${req.path}`);
+import winston from 'winston'
+const { combine, timestamp, errors, json, cli } = winston.format
+export const logger = winston.createLogger({
+    level: "info",
+    format: combine(timestamp(), errors({ stack: true }), cli()),
+    // format: winston.format.cli(), //switch to json()
+    transports: [new winston.transports.Console(),
+    new winston.transports.File({ filename: "standard.log" })
+    ],
+    exceptionHandlers: [
+        new winston.transports.File({ filename: "exception.log" })
+    ],
+    rejectionHandlers: [
+        new winston.transports.File({ filename: "rejections.log" })
+    ]
+})
+export function req_info(req: Request, res: Response, next: NextFunction) {
+    logger.info(`${req.method} ${req.path}`)
+    // winston.log("info", `${req.method} ${req.path}`)
+    // console.log(`${req.method} ${req.path}`);
     next()
 }
