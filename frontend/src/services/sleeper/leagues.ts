71728abe82b488c9f6d4ab6d73cb6383b0ad956f
jordanlkfung <jordanlkfung@gmail.com>
2025-06-10 23:46:57 -0700

    renamed apiget to sleeper_api_get

--- frontend/src/services/sleeper/leagues.ts
+++ frontend/src/services/sleeper/leagues.ts
@@ -1,19 +1,19 @@
-import { apiGet } from './apiClient';
+import { sleeper_apiGet } from './apiClient';
 import { getPlayer } from './player'
 import type { League, LeagueInfo, Roster, User, Player, Players } from './types';
 
 export const getLeaguesForUser = async (username: string, season: string): Promise<League[]> => {
-    const user: User = await apiGet(`/user/${username}`)
-    return apiGet<League[]>(`/user/${user.user_id}/leagues/nba/${season}`);
+    const user: User = await sleeper_apiGet(`/user/${username}`)
+    return sleeper_apiGet<League[]>(`/user/${user.user_id}/leagues/nba/${season}`);
 }
 
 export const getRosters = async (leagueId: string): Promise<Roster[]> => {
-    return await apiGet<Roster[]>(`/league/${leagueId}/rosters`)
+    return await sleeper_apiGet<Roster[]>(`/league/${leagueId}/rosters`)
 }
 
 
 export const getLeagueInfo = async (leagueId: string): Promise<LeagueInfo> => {
-    return await apiGet<LeagueInfo>(`/league/${leagueId}`)
+    return await sleeper_apiGet<LeagueInfo>(`/league/${leagueId}`)
 }
 
 export const getPlayersForRosters = async (leagueId: string): Promise<Record<string, Player[]>> => {
@@ -72,8 +72,8 @@ export interface TeamInfo {
     roster_id: number
 }
 export const getTeamInfo = async (leagueId: string): Promise<TeamInfo[]> => {
-    const rosters = await apiGet<TeamSettings[]>(`/league/${leagueId}/rosters`)
-    const users = await apiGet<leagueUser[]>(`/league/${leagueId}/users`)
+    const rosters = await sleeper_apiGet<TeamSettings[]>(`/league/${leagueId}/rosters`)
+    const users = await sleeper_apiGet<leagueUser[]>(`/league/${leagueId}/users`)
     console.log(rosters)
     console.log(users)
     //returns array of dict, dict constains users
