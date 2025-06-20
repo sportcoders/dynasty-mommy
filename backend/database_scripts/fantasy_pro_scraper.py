51169429491ef69f5277c6e3a096f8545ddb26d9
jordanlkfung <jordanlkfung@gmail.com>
2025-06-19 20:34:28 -0700

    espn, yahoo and cat ranking now all functional

--- backend/database_scripts/fantasy_pro_scraper.py
+++ backend/database_scripts/fantasy_pro_scraper.py
@@ -1,24 +1,47 @@
 import os
-from httpx import head
+from time import sleep
 import pymongo
 from playwright.sync_api import sync_playwright
 from playwright_stealth import Stealth
 from dotenv import load_dotenv
 
 DB_NAME = 'test'
-
+CAT_ROTO_RANKING_COLLECTION = 'fantasy_pro_ranking_cat_roto'
+YAHOO_PTS_RANKING_COLLECTION = 'fantasy_pro_ranking_yahoo_pts'
+ESPN_PTS_RANKING_COLLECTION = 'fantasy_pro_ranking_espn_pts'
+def getRankingsFromTable(page, db):
+    table = page.locator('table')
+    rows = table.locator('tr').all()
+    insert_list = []
+    for row in rows:
+        cells = row.locator('td').all()
+        if len(cells) >2:
+            rank = cells[0].inner_text() #fantasy pro ranking
+            player_info = cells[2].inner_text() #player info
+            name, teaminfo = player_info.split("(")
+            teaminfo = teaminfo.split(")")[0]
+            team, positions = teaminfo.split('-')
+            team.strip()
+            positions.strip()
+            positions = positions.split(',')
+            print(rank, name, team, positions)
+            insert_list.append({"rank":rank, 
+                                "name":name,
+                                "team":team,
+                                "position":positions
+                                })
+    # print(insert_list)
+    db.insert_many(insert_list)
 
 def main():
     load_dotenv()
     URI = os.getenv("DB_URI")
     client = pymongo.MongoClient(URI)
     db = client[DB_NAME]
-    print(db.list_collection_names())
-    if 'fantasy_pro_ranking_yahoo_pts' in db.list_collection_names():
-        drop = db['fantasy_pro_ranking_yahoo_pts']
-        drop.drop()
-    db = db['fantasy_pro_ranking_cat_roto']
-    insert_list = []
+    for collection in [YAHOO_PTS_RANKING_COLLECTION, ESPN_PTS_RANKING_COLLECTION, CAT_ROTO_RANKING_COLLECTION]:
+        if collection in db.list_collection_names():
+            drop = db[collection]
+            drop.drop()
     with  Stealth().use_sync(sync_playwright()) as p:
         browser = p.chromium.launch(headless=False)
         # context = browser.new_context(
@@ -29,27 +52,20 @@ def main():
         # )
         page = browser.new_page()
         page.goto('https://www.fantasypros.com/nba/rankings/dynasty-overall.php')
-        page.wait_for_selector('table', timeout=10000)
-        table = page.locator('table')
-        rows = table.locator('tr').all()
-        for row in rows:
-            cells = row.locator('td').all()
-            if len(cells) >2:
-                rank = cells[0].inner_text() #fantasy pro ranking
-                player_info = cells[2].inner_text() #player info
-                name, teaminfo = player_info.split("(")
-                teaminfo = teaminfo.split(")")[0]
-                team, positions = teaminfo.split('-')
-                team.strip()
-                positions.strip()
-                # print(rank, name, team, positions)
-                positions = positions.split(',')
-                insert_list.append({"rank":rank, 
-                                    "name":name,
-                                    "team":team,
-                                    "position":positions
-                                    })
-    db.insert_many(insert_list)
+        page.wait_for_selector('table', timeout=5000)
+
+        getRankingsFromTable(page, db[CAT_ROTO_RANKING_COLLECTION])
+        button = page.locator('.select-advanced__button').nth(1)
+        button.wait_for(state='visible')
+        button.click()
+        page.click('text=Points (Yahoo)')
+        page.wait_for_selector('table', timeout=5000)
+        getRankingsFromTable(page, db[YAHOO_PTS_RANKING_COLLECTION])
+        button.click()
+        page.click('text=Points (ESPN)')
+        page.wait_for_selector('table', timeout=5000)
+        getRankingsFromTable(page, db[ESPN_PTS_RANKING_COLLECTION])
+
     
 if __name__ =='__main__':
     main()
