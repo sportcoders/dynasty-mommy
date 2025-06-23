0cfa0e62e571a7f0c8fe7d67745b2080defe944a
jordanlkfung <jordanlkfung@gmail.com>
2025-06-23 11:45:12 -0700

    added routes for user

---
+++ backend/src/routes/user.ts
@@ -0,0 +1,7 @@
+import { Router } from "express";
+import * as user_controller from "../controller/user"
+const user_router = Router()
+
+user_router.route("/login").post((req, res, next) => { user_controller.login(req, res, next) })
+user_router.route("/signup").post((req, res, next) => { user_controller.signUp(req, res, next) })
+user_router.route("/addLeague").post((req, res, next) => { user_controller.addLeagueToUser(req, res, next) })
