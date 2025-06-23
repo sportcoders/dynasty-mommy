1ad73e449fe45b6c48f6a1949b6425e90feb2cfc
jordanlkfung <jordanlkfung@gmail.com>
2025-06-23 11:51:41 -0700

    added middleware to addleague route

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import * as user_controller from "../controller/user"
+import { authenticate } from "../middleware/authenticate";
 const user_router = Router()
 
 user_router.route("/login").post((req, res, next) => { user_controller.login(req, res, next) })
 user_router.route("/signup").post((req, res, next) => { user_controller.signUp(req, res, next) })
-user_router.route("/addLeague").post((req, res, next) => { user_controller.addLeagueToUser(req, res, next) })
+user_router.route("/addLeague").post(authenticate, (req, res, next) => { user_controller.addLeagueToUser(req, res, next) })
