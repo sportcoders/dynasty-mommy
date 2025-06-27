92a23cc36302c47adee6a09d0d170660d8290834
jordanlkfung <jordanlkfung@gmail.com>
2025-06-26 21:08:34 -0700

    Modified options for sqlite test db

--- backend/src/orm.ts
+++ backend/src/orm.ts
@@ -1,19 +1,32 @@
-import { DataSource } from "typeorm";
+import { DataSource, DataSourceOptions } from "typeorm";
 import { User, UserLeagues } from "./models/user";
 
 export function createAppDataSource(url: string): DataSource {
-    return new DataSource({
-        type: "postgres",
-        url: url,
-        // host: "localhost",
-        // port: 5432,
-        // username: "test",
-        // password: "test",
-        // database: "test",
-        // synchronize: true,
-        logging: true,
-        entities: [User, UserLeagues],
-        // subscribers: [],
-        // migrations: [],
-    })
+    let options: DataSourceOptions;
+
+    if (url == 'sqlite') {
+        options = {
+            type: "sqlite",
+            database: ":memory:",
+            synchronize: true,
+            dropSchema: true,
+            logging: false
+        }
+    }
+    else {
+        options = {
+            type: 'postgres',
+            // host: "localhost",
+            // port: 5432,
+            // username: "test",
+            // password: "test",
+            // database: "test",
+            // synchronize: true,
+            // subscribers: [],
+            // migrations: [],
+            logging: true
+        }
+    }
+    options = { ...options, entities: [User, UserLeagues] }
+    return new DataSource(options)
 }
