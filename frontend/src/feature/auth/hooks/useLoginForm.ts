4f28631e48797cbdcec3aceca5a45be8ad64ae44
jordanlkfung <jordanlkfung@gmail.com>
2025-07-02 12:23:54 -0700

    form can now access token

--- frontend/src/feature/auth/hooks/useLoginForm.ts
+++ frontend/src/feature/auth/hooks/useLoginForm.ts
@@ -1,3 +1,4 @@
+import { DM_login } from "@services/dynasty-mommy/user"
 import { useState } from "react"
 
 export default function useLoginForm() {
@@ -10,16 +11,20 @@ export default function useLoginForm() {
         setEmailError("")
         setEmail(e.target.value)
     }
-    const handleSubmit = () => {
+    const handleSubmit = async () => {
         setEmailError("")
         setError("")
         if (!email || !password) {
             setError("Missing Fields")
             return
         }
-        if (!email.toLowerCase().match(reEmail)) {
-            setEmailError("Invalid Email Format")
-            return
+        // if (!email.toLowerCase().match(reEmail)) {
+        //     setEmailError("Invalid Email Format")
+        //     return
+        // }
+        const headers = await DM_login(email, password)
+        if (headers) {
+            console.log(headers.get('Authentication'))
         }
     }
     return { email, handleEmailChange, password, setPassword, handleSubmit, emailError, error }
