9a3975489948349edd775d154bbc2b73cdf4c441
jordanlkfung <jordanlkfung@gmail.com>
2025-07-02 22:44:26 -0700

    added signup route to service

--- frontend/src/services/dynasty-mommy/user.ts
+++ frontend/src/services/dynasty-mommy/user.ts
@@ -9,3 +9,12 @@ export async function DM_login(email: string, password: string) {
         console.log(e)
     }
 }
+export async function DM_signup(username: string, email: string, password: string) {
+    try {
+        const { headers } = await serverPost('/auth/signup', { username: username, email: email, password: password })
+        return headers
+    }
+    catch (e) {
+        console.log(e)
+    }
+}
