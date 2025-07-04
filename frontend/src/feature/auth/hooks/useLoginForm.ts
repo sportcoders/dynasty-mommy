e5887497cd8ccb96f7c3fd17e0b860f8c7935bd7
jordanlkfung <jordanlkfung@gmail.com>
2025-07-03 21:54:55 -0700

    token is not set

--- frontend/src/feature/auth/hooks/useLoginForm.ts
+++ frontend/src/feature/auth/hooks/useLoginForm.ts
@@ -1,5 +1,8 @@
 import { DM_login } from "@services/dynasty-mommy/user"
 import { useState } from "react"
+import { useDispatch } from "react-redux"
+import { login } from "../authSlice"
+import { useAppDispatch } from "@app/hooks"
 
 export default function useLoginForm() {
     const reEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
@@ -7,6 +10,8 @@ export default function useLoginForm() {
     const [password, setPassword] = useState("")
     const [emailError, setEmailError] = useState("")
     const [error, setError] = useState("")
+    const dispatch = useAppDispatch()
+
     const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setEmailError("")
         setEmail(e.target.value)
@@ -24,7 +29,9 @@ export default function useLoginForm() {
         }
         const headers = await DM_login(email, password)
         if (headers) {
+            const token = headers.get('Authentication')
             console.log(headers.get('Authentication'))
+            dispatch(login(token))
         }
     }
     return { email, handleEmailChange, password, setPassword, handleSubmit, emailError, error }
