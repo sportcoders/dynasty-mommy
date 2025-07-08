dbc54f7beeadb73938322172484e3c4f18d54c8b
jordanlkfung <jordanlkfung@gmail.com>
2025-07-08 11:52:08 -0700

    added redux persist

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,9 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist"
+import storage from "redux-persist/lib/storage";
 import authReducer from "@feature/auth/authSlice"
+
+const persistConfig = {
+    key: "root",
+    storage,
+}
+const persistedReducer = persistReducer(persistConfig, authReducer)
 export const store = configureStore({
     reducer: {
-        authReducer
+        authReducer: persistedReducer
     }
 })
+export const persistor = persistStore(store)
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
