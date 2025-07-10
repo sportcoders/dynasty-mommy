8fca3c66b4d5199c295281f7200b86ac7f279efa
jordanlkfung <jordanlkfung@gmail.com>
2025-07-09 22:23:21 -0700

    removed username frome request

--- backend/src/middleware/authenticate.ts
+++ backend/src/middleware/authenticate.ts
@@ -2,7 +2,6 @@ import { NextFunction, Request, Response } from "express";
 import { HttpError } from "../constants/constants";
 import { AppError } from "../errors/app_error";
 import { verifyToken } from "../utils/jwt";
-import UserSession from "../models/session";
 
 declare global {
     namespace Express {
@@ -10,7 +9,6 @@ declare global {
             user?: {
                 user_id?: string;
                 email: string;
-                username?: string;
             }
         }
     }
@@ -24,11 +22,7 @@ export const authenticate = async (req: Request, res: Response, next: NextFuncti
 
         if (error || !payload || payload.type == 'refresh') throw new AppError({ statusCode: HttpError.UNAUTHORIZED, message: error === "jwt expired" ? "Token expired" : "Invalid token" });
 
-        // const validSession = await UserSession.findById(payload.id)
-        // //check date of session
-        // if (!validSession || validSession.expiresAt < new Date()) throw new AppError({ statusCode: HttpError.UNAUTHORIZED, message: "missing/expired session" });
-
-        req.user = { email: payload.email, user_id: "test", username: "test" }
+        req.user = { email: payload.email, user_id: "test" }
 
         next();
 
