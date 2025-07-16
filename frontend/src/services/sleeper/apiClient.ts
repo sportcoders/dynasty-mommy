e60c13fd92a9f38f2b2d8d153a01282cac5734bd
kevinboc <k.boc@yahoo.com>
2025-07-15 21:08:52 -0700

    DM-76 Moved URLs to Vite Environment Variables

--- frontend/src/services/sleeper/apiClient.ts
+++ frontend/src/services/sleeper/apiClient.ts
@@ -1,57 +1,59 @@
 import { useAppDispatch } from "@app/hooks";
 import { logout } from "@feature/auth/authSlice";
-const BASE_URL = 'https://api.sleeper.app/v1';
-const SERVER_BASE_URL = 'http://localhost:8000'
-const AVATAR_URL = 'https://sleepercdn.com/avatars'
+
+const SERVER_BASE_URL = import.meta.env.VITE_SERVER_URL;
+const BASE_URL = import.meta.env.VITE_SLEEPER_URL;
+const AVATAR_URL = import.meta.env.VITE_SLEEPER_AVATAR_URL;
+
 export const sleeper_apiGet = async <T>(endpoint: string): Promise<T> => {
     const response = await fetch(`${BASE_URL}${endpoint}?ts=${Date.now()}`, {
-        cache: 'no-store'
+        cache: "no-store",
     });
 
     if (!response.ok) {
-        throw new Error(`API request failed: ${response.status} ${response.statusText}`);
+        throw new Error(
+            `API request failed: ${response.status} ${response.statusText}`
+        );
     }
 
     return response.json() as Promise<T>;
-}
+};
 
 export const serverGet = async <T>(endpoint: string): Promise<T> => {
-    const response = await fetch(`${SERVER_BASE_URL}${endpoint}`,
-        { credentials: "include" }
-    )
+    const response = await fetch(`${SERVER_BASE_URL}${endpoint}`, {
+        credentials: "include",
+    });
 
-    return response.json() as Promise<T>
-}
-export const serverPost = async <T, U>(endpoint: string, data: U): Promise<T> => {
-    const response = await fetch(`${SERVER_BASE_URL}${endpoint}`,
-        {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(data),
-            credentials: "include"
-        }
-    )
-    return response.json() as Promise<T>
-}
+    return response.json() as Promise<T>;
+};
+export const serverPost = async <T, U>(
+    endpoint: string,
+    data: U
+): Promise<T> => {
+    const response = await fetch(`${SERVER_BASE_URL}${endpoint}`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+        credentials: "include",
+    });
+    return response.json() as Promise<T>;
+};
 export const serverProtectedPost = async <T>(req: Request): Promise<T> => {
-
-    const response = await fetch(req)
+    const response = await fetch(req);
     if (response.status == 401) {
-        const newTokenReq = await fetch("REFRESHROUTE")
+        const newTokenReq = await fetch("REFRESHROUTE");
         if (newTokenReq.status == 401) {
-            const dispatch = useAppDispatch()
-            dispatch(logout())
-            throw new Error("Session Expired, Please Login Again")
+            const dispatch = useAppDispatch();
+            dispatch(logout());
+            throw new Error("Session Expired, Please Login Again");
         }
-        const retryReq = await fetch(req)
-        return retryReq.json() as Promise<T>
+        const retryReq = await fetch(req);
+        return retryReq.json() as Promise<T>;
     }
-    return response.json() as Promise<T>
-
-}
+    return response.json() as Promise<T>;
+};
 export const sleeper_avatarGet = async <T>(endpoint: string): Promise<T> => {
-    const response = await fetch(`${AVATAR_URL}${endpoint}`)
+    const response = await fetch(`${AVATAR_URL}${endpoint}`);
 
-
-    return response.blob() as Promise<T>
-}
+    return response.blob() as Promise<T>;
+};
