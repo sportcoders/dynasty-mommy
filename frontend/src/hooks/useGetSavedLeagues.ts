9f5cdca335d63e552ac4bb9022162b9591473b3f
jordanlkfung <jordanlkfung@gmail.com>
2025-07-16 21:02:34 -0700

    added global hook to fetch api for user leagues

---
+++ frontend/src/hooks/useGetSavedLeagues.ts
@@ -0,0 +1,16 @@
+import { useAppSelector } from '@app/hooks'
+import { fetchUserLeagues } from '@services/api/user'
+import { useQuery } from '@tanstack/react-query'
+
+
+export function useGetSavedLeagues() {
+    const username = useAppSelector(state => state.authReducer.username)
+    const { isPending, isError, data, error } = useQuery({
+        queryKey: ['userSavedLeagues'],
+        queryFn: fetchUserLeagues,
+        enabled: !!username,
+        select: (leagues) => leagues?.leagues
+    })
+
+    return { isPending, isError, data, error }
+}
