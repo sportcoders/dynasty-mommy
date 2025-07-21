9ba83472d5390d6a9a41f20015cc4d519595c364
kevinboc <k.boc@yahoo.com>
2025-07-20 22:43:20 -0700

    DM-49 Removed try/catch from service functions

--- frontend/src/services/sleeper/avatar.ts
+++ frontend/src/services/sleeper/avatar.ts
@@ -7,13 +7,7 @@ import { sleeper_avatarGet } from "./apiClient";
  * @returns a Blob object of the avatar or null
  */
 export async function sleeper_getAvatarThumbnail(avatar_id: string): Promise<Blob | null> {
-    try {
-        const avatar: Blob = await sleeper_avatarGet(`/thumbs/${avatar_id}`)
-
-        return avatar   
-    } catch (error) {
-        console.error('Failed to fetch avatar:', error);
-        return null; 
-    }
+    const avatar: Blob = await sleeper_avatarGet(`/thumbs/${avatar_id}`);
+    return avatar;
 }
 
--- frontend/src/services/sleeper/player.ts
+++ frontend/src/services/sleeper/player.ts
@@ -1,5 +1,5 @@
 import { serverGet } from "./apiClient";
-import type { Player, Players} from "./types";
+import type { Player, Players } from "./types";
 
 /**
  * Function to get Player object(s) from the backend API.
@@ -8,16 +8,16 @@ import type { Player, Players} from "./types";
  * @returns an array of Player objects
  */
 export const sleeper_getPlayer = async (playerId: string): Promise<Player[]> => {
-    try {
-        const players = await serverGet<Players>(`/sleeper_player/${playerId}`);
-        return players.players.map((player: Player) => ({
-            id: player.id,
-            first_name: player.first_name,
-            last_name: player.last_name,
-            position: player.position
-        }));
-    } catch (error) {
-        console.error('Failed to fetch player(s):', error);
+    const players = await serverGet<Players>(`/sleeper_player/${playerId}`);
+
+    if (!players || !players.players) {
         return [];
     }
+
+    return players.players.map((player: Player) => ({
+        id: player.id,
+        first_name: player.first_name,
+        last_name: player.last_name,
+        position: player.position
+    }));
 };
