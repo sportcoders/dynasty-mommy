568b625f1e05683a98ace3d3689f3f62698950b1
jordanlkfung <jordanlkfung@gmail.com>
2025-07-23 22:09:12 -0700

    DM-50 added patch to cors options

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,37 +3,37 @@ import player_sleeper_router from './routes/player_sleeper';
 import { req_info } from './middleware/logger';
 import { errorHandler } from './middleware/error_handler';
 import { invalid_endpoint } from './errors/endpoint_not_found';
-import cors from 'cors'
+import cors from 'cors';
 import user_router from './routes/user';
-import "reflect-metadata"
+import "reflect-metadata";
 import { DataSource } from 'typeorm';
 import cookieParser from 'cookie-parser';
 import auth_router from './routes/auth';
 const corsOptions = {
     origin: 'http://localhost:5173',
-    methods: ['GET', 'POST', 'OPTIONS', 'DELETE'],
+    methods: ['GET', 'POST', 'OPTIONS', 'DELETE', 'PATCH'],
     credentials: true
 };
 
 let AppDataSource: DataSource;
 export function init_app(dataSource: DataSource) {
     const app = express();
-    AppDataSource = dataSource
+    AppDataSource = dataSource;
 
     app.use(cors(corsOptions));
-    app.use(cookieParser())
+    app.use(cookieParser());
 
-    app.use(express.json())
-    app.use(req_info)
-    app.use('/sleeper_player', player_sleeper_router)
-    app.use('/auth', auth_router)
-    app.use('/user', user_router)
+    app.use(express.json());
+    app.use(req_info);
+    app.use('/sleeper_player', player_sleeper_router);
+    app.use('/auth', auth_router);
+    app.use('/user', user_router);
     app.get('/healthcheck', (req, res) => {
         res.status(200).json({ message: "Healthy" });
     });
-    app.use(invalid_endpoint)
-    app.use(errorHandler)
+    app.use(invalid_endpoint);
+    app.use(errorHandler);
 
-    return app
+    return app;
 }
-export { AppDataSource }
+export { AppDataSource };
