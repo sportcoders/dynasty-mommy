575c45e944158fc4669a922ba647e83d6bac8b36
kevinboc <k.boc@yahoo.com>
2025-08-03 21:42:39 -0700

    DM-100 Added more comments to hook

--- frontend/src/feature/leagues/hooks/useSleeperPlayers.ts
+++ frontend/src/feature/leagues/hooks/useSleeperPlayers.ts
@@ -4,8 +4,11 @@ import { useQuery } from "@tanstack/react-query";
 /**
  * Fetches players using Sleeper API
  * @param leagueId - id of the Sleeper League
- * @returns
- */
+ * @returns {Object} Query result object
+ * @returns {Player[] | undefined} returns.data - Array of player data from Sleeper API, undefined while loading or on error
+ * @returns {boolean} returns.isLoading - Loading state of the query
+ * @returns {Error | null} returns.error - Error object if query fails, null otherwise
+ */;
 export default function useSleeperPlayers(leagueId: string) {
     const { data, isLoading, error } = useQuery({
         queryKey: ['league', leagueId],
@@ -16,6 +19,7 @@ export default function useSleeperPlayers(leagueId: string) {
         gcTime: 15 * 60 * 1000,            // cached 15 min after unused
         enabled: !!leagueId,               // wait until leagueId is defined
         // TODO: Maybe add the 'select' option to add new metadata for new added/dropped players to show on the UI if needed
+        // onSuccess and onError depracted in v5
     });
 
     return { data, isLoading, error };
