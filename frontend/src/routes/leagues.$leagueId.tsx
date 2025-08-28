73bedd6cc852dedd3a1d02ab7d73665bc8c40ba6
jordanlkfung <jordanlkfung@gmail.com>
2025-08-28 10:03:07 -0700

    added zod for validating params

--- frontend/src/routes/leagues.$leagueId.tsx
+++ frontend/src/routes/leagues.$leagueId.tsx
@@ -1,19 +1,11 @@
 import LeagueHome from '@pages/LeaguesHome';
 import { createFileRoute } from '@tanstack/react-router';
+import { z } from 'zod';
 
-interface LeagueHomeSearch {
-    tab?: number;
-    parent?: string;
-}
 export const Route = createFileRoute('/leagues/$leagueId')({
     component: LeagueHome,
-    validateSearch: (search: Record<string, unknown>): LeagueHomeSearch => {
-        // validate and parse the search params into a typed state
-        return {
-            tab: Number(search.tab ?? 0),
-            parent: (search.parent as string) || '',
-            //   sort: (search.sort as ProductSearchSortOptions) || 'newest',
-        };
-    },
-})
-
+    validateSearch: z.object({
+        tab: z.number().optional(),
+        parent: z.string().optional(),
+    }),
+});
