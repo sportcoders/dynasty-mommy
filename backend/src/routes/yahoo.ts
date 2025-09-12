cdc43bb757582761e1443401a9b21cf8a0876925
jordanlkfung <jordanlkfung@gmail.com>
2025-09-12 10:42:01 -0700

    DM-172 added controllers and routes for yahoo league CRUD operations

--- backend/src/routes/yahoo.ts
+++ backend/src/routes/yahoo.ts
@@ -9,5 +9,7 @@ yahoo_router.route("/leagues").get(yahoo_controller.getLeagues);
 yahoo_router.route("/leagues/:league_key/teams").get(yahoo_controller.getLeagueAndTeams);
 yahoo_router.route("/roster/:team_key").get(yahoo_controller.getRoster);
 yahoo_router.route("/unlink").delete(yahoo_controller.unlinkYahoo);
-
+yahoo_router.route("/league").post(yahoo_controller.saveLeague);
+yahoo_router.route("/league/:league_key").delete(yahoo_controller.removeLeague);
+yahoo_router.route("/league/:league_key").get(yahoo_controller.getLeague);
 export default yahoo_router;
