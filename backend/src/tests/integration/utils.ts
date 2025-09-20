e4d9fe273e69b80af7d329f0c93bcaa19d9d6e21
jordanlkfung <jordanlkfung@gmail.com>
2025-09-19 20:39:28 -0700

    DM-188 backend tests for yahoo endpoints that use only our backend complete

--- backend/src/tests/integration/utils.ts
+++ backend/src/tests/integration/utils.ts
@@ -45,3 +45,11 @@ export const users = [
         leagues: []
     }
 ];
+export const yahooLeagues = [{
+    league_key: "",
+    user_id: ""
+}];
+const sleeper_leagues = [
+    { "platform": "sleeper", "league_id": "111333444", saved_user: "12131313" },
+    { "platform": "sleeper", "league_id": "1e322", saved_user: "12131313" }
+];
