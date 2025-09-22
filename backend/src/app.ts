5b5da87b2a20e6684ce0fade1d7ff4960647e104
jordanlkfung <jordanlkfung@gmail.com>
2025-09-21 20:15:15 -0700

    DM-193 created route to get trade market for sleeper

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -11,6 +11,7 @@ import cookieParser from 'cookie-parser';
 import auth_router from './routes/auth';
 import sleeper_league_router from './routes/sleeper_league';
 import yahoo_router from './routes/yahoo';
+import sleeper_trade_market_router from './routes/sleeper_trade_market';
 const corsOptions = {
     origin: ['http://localhost:5173', 'https://localhost:5173'],
     methods: ['GET', 'POST', 'OPTIONS', 'DELETE', 'PATCH'],
@@ -32,6 +33,7 @@ export function init_app(dataSource: DataSource) {
     app.use('/user', user_router);
     app.use('/sleeper_league', sleeper_league_router);
     app.use('/yahoo', yahoo_router);
+    app.use('/sleeper_trade_market', sleeper_trade_market_router);
     app.get('/healthcheck', (req, res) => {
         res.status(200).json({ message: "Healthy" });
     });
